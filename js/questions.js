23d03b449ee491d49c3a3fd8525aa3a0e044962a
Pratap Pawar <pratappawar1997@gmail.com>
2021-08-12 19:54:52 +0530

    Added: Online Test Application

---
+++ js/questions.js
@@ -0,0 +1,58 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Who is the prime minister of India?",
+    answer: "Narendra Modi",
+    options: [
+      "Narendra Modi",
+      "Arun Jetli",
+      "Nitin Gadkari",
+      "Amit Shah"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What name of your country?",
+    answer: "India",
+    options: [
+      "America",
+      "Japan",
+      "India",
+      "China"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+];
